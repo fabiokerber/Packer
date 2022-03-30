2278ebc225ac148a802168ad1011a519ff80f467
Fabio Kerber Silva <fkerbers@emeal.nttdata.com>
2022-03-30 15:42:48 -0300

    Atualizacao codigo

--- 1.zbx_awx_sh/packer/v4/script/awx.sh
+++ 1.zbx_awx_sh/packer/v4/script/awx.sh
@@ -4,5 +4,8 @@ sudo yum update -y
 sudo yum install -y rh-python38-python.x86_64 rh-python38-python-pip.noarch rh-python38-python-devel.x86_64 '@Development Tools' git libselinux-python3.x86_64
 sudo pip3.6 install --upgrade pip && pip3.6 install --upgrade setuptools
 sudo pip3.6 install setuptools_rust wheel && sudo pip3.6 install ansible
-sudo mkdir -p /etc/ansible
 mkdir -p ~/git
+git clone https://github.com/fabiokerber/Packer.git ~/git
+sudo mkdir -p /etc/ansible
+sudo cp -R ~/git/1.zbx_awx_sh/packer/v5/ansible/* /etc/ansible/
+ansible-playbook /etc/ansible/playbook.yml
