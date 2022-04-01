07ff42938292701fcfca3149694d60da475357ab
Fabio Kerber Silva <fkerbers@emeal.nttdata.com>
2022-04-01 11:45:47 -0300

    Atualizacao codigo

--- 1.zbx_awx_sh/packer/v4/script/awx.sh
+++ 1.zbx_awx_sh/packer/v4/script/awx.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo timedatectl set-timezone America/Sao_Paulo
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum update -y
-sudo yum install -y rh-python38-python.x86_64 rh-python38-python-pip.noarch rh-python38-python-devel.x86_64 '@Development Tools' git libselinux-python3.x86_64
-sudo pip3.6 install --upgrade pip && pip3.6 install --upgrade setuptools
+sudo yum install -y packer yum-utils rh-python38-python.x86_64 rh-python38-python-pip.noarch rh-python38-python-devel.x86_64 '@Development Tools' git libselinux-python3.x86_64
+sudo pip3.6 install --upgrade pip && sudo pip3.6 install --upgrade setuptools
 sudo pip3.6 install setuptools_rust wheel && sudo pip3.6 install ansible
 mkdir -p ~/git
 git clone https://github.com/fabiokerber/Packer.git ~/git
--- 1.zbx_awx_sh/packer/v5/builder.pkr.hcl
+++ 1.zbx_awx_sh/packer/v5/builder.pkr.hcl
@@ -2,7 +2,7 @@ build {
   name    = "builder"
   sources = ["source.azure-arm.awx"]
   provisioner "ansible" {
-    playbook_file = "../ansible/playbook.yml"
+    playbook_file = "ansible/playbook.yml"
     pause_before  = "1m"
     timeout       = "40m"
   }
